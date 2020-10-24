34cfc581a9c70a3e1a22321707e10b537ddbac40
Ильнур Курмандаев <59246442+KIlnourik@users.noreply.github.com>
2020-10-24 18:30:14 +0500

    Готовый вариант

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const formButton = document.querySelector(".form-opening-button");
+const searchForm = document.querySelector(".hotel-search-form");
+const arrivalDateInput = document.querySelector(".arrival-date-input");
+const departureDateInput = document.querySelector(".departure-date-input");
+const adultInput = document.querySelector(".adults-input");
+const childrenInput = document.querySelector(".children-input");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+    storage = localStorage.getItem("adults");
+    storage = localStorage.getItem("chidren");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+formButton.addEventListener('click', function () {
+    searchForm.classList.toggle("search-form-hide");
+    searchForm.classList.toggle("search-form-open");
+});
+
+searchForm.addEventListener("submit", function (evt) {
+    if (!arrivalDateInput.value || !departureDateInput.value || !adultInput.value || !childrenInput.value) {
+        evt.preventDefault();
+        searchForm.classList.toggle("search-form-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("adults", adultInput.value);
+            localStorage.setItem("chidren", childrenInput.value);
+        }
+        
+    }
+})
+
